695ffe538bbc2da80d7f9fe9512bddba31eee3ea
Derek Wheeler <frozenarrow73@gmail.com>
2023-04-14 11:24:48 -0600

    completed assignment for week

--- src/App.js
+++ src/App.js
@@ -6,6 +6,26 @@ function App() {
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""])
   const [player, setPlayer] = useState(true)
 
+  const calculateWinner = (arr) => {
+    let list = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < list.length; i++) {
+      const [a, b, c] = list[i]
+      if (arr[a] === arr[b] && arr[a] === arr[c] && arr[a] !== "") {
+        return `${arr[a]} won the game!`
+      }
+    }
+    return "Who will win?"
+  }
+
   return (
     <div className="App">
       <div className='container'>
@@ -20,6 +40,7 @@ function App() {
         />
         })}
       </div>
+      <span>{calculateWinner(squares)}</span>
     </div>
   );
 }
